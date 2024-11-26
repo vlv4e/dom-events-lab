990af713df438337e6781494a85962f932d8ea33
vlv4e <mhndbinbader@gmail.com>
2024-11-26 18:23:02 +0300

    0

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 /*-------------------------------- Constants --------------------------------*/
+const buttons = document.querySelectorAll('.button');
+const calculator = document.querySelector('#calculator');
+
 
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+calculator.addEventListener('click', (event) => {
+    // This log is for testing purposes to verify we're getting the correct value
+    // You have to click a button to see this log
+    console.log(event.target.innerText);
+  
+    // Example
+    if (event.target.classList.contains('number')) {
+      // Do something with a number
+    }
+  
+    // Example
+    if (event.target.innerText === '*') {
+      // Do something with this operator
+    }
+  });
+  
 /*-------------------------------- Functions --------------------------------*/
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      // This log is for testing purposes to verify we're getting the correct value
+      console.log(event.target.innerText);
+      // Future logic to capture the button's value would go here...
+    });
+  });
+  
